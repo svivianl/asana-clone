d720ebcdefb1a3114861eb8d2225347b1931e412
Viviane <vshibukawa@spiria.com>
2020-03-25 09:57:31 -0400

    add sass package

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./css/test.css";
 
 function App() {
   return (
     <div className="App">
-
+      <h1 className="h1-sass">Helloooooooooooo</h1>
     </div>
   );
 }
