ca34142a8fec38361df9e1259ccb7f62d96421e8
Viviane <vshibukawa@spiria.com>
2020-03-26 15:47:01 -0400

    Task component - initial solution

--- client/src/components/features/application/components/workArea/components/list/components/projects/components/tasks/components/task/Task.tsx
+++ client/src/components/features/application/components/workArea/components/list/components/projects/components/tasks/components/task/Task.tsx
@@ -1,66 +1,35 @@
-import React, { useEffect, useState } from 'react';
-
-interface Task{
-    id: string;
-    assignee: string;
-    title: string;
-    description: string;
-    status: string;
-    dueDate: Date;
-    creationDate: Date;
-    project: string;
+import React, { useEffect, useState } from "react";
+import TaskDetailsForm from "./components/TaskDetails/TaskDetails";
+interface Task {
+  id: string;
+  assignee: string;
+  title: string;
+  description: string;
+  status: string;
+  dueDate: Date;
+  creationDate: Date;
+  project: string;
 }
 
-interface TaskProps{
-    task: Task;
+interface TaskProps {
+  task?: Task;
 }
 
 const Task = ({ task }: TaskProps) => {
-    // const [assignee, setAssignee] = useState({})
-    const { 
-        // id,
-        assignee = "Juninhos",
-        title = "Does this work?",
-        description = "Most awesome project ever built",
-        // status,
-        dueDate = Date.now(),
-        // creationDate,
-        project = "Asana Clone"
-    } = task;
+  // const [assignee, setAssignee] = useState({})
 
-    // useEffect(() => {
+  // useEffect(() => {
 
-    // }, [])
+  // }, [])
 
-    return(
-        <div className='jumbotron'>
-            {/* TODO: status bar */}
-            <div className='container'>
-              <h4>{title}</h4>
-              <div>
-                <h5>Assignee</h5>
-                <span>
-                  <img />
-                  <h5>{assignee}</h5>
-                </span>
-              </div>
-              <div>
-                  <h5>Due date</h5>
-                  <span>{dueDate}</span>
-              </div>
-              <div>
-                  <h5>Projects</h5>
-                  <span>{project}</span>
-              </div>
-              <div>
-                  <h5>Description</h5>
-                  <textarea>{description}</textarea>
-              </div>
-            </div>
-            
-            {/* TODO: comments bar */}
-        </div>
-    )
+  return (
+    <div>
+      {/* TODO: status bar */}
+      <TaskDetailsForm task={task} />
+
+      {/* TODO: comments bar */}
+    </div>
+  );
 };
 
 export default Task;
--- client/src/components/features/application/components/workArea/components/list/components/projects/components/tasks/components/task/components/TaskDetails/TaskDetails.tsx
+++ client/src/components/features/application/components/workArea/components/list/components/projects/components/tasks/components/task/components/TaskDetails/TaskDetails.tsx
@@ -1,13 +1,113 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-const TaskDetails = () => {
+interface Task {
+  id: string;
+  assignee: string;
+  title: string;
+  description: string;
+  status: string;
+  dueDate: Date;
+  creationDate: Date;
+  project: string;
+}
 
+interface TaskDetailsProps {
+  task?: Task;
+}
 
-    return(
-        <div className='jumbotron'>
-           
+const taskInitialValues: Task = {
+  id: "",
+  assignee: "",
+  title: "",
+  description: "",
+  status: "",
+  dueDate: new Date(),
+  creationDate: new Date(),
+  project: ""
+};
+
+const TaskDetails = ({ task = taskInitialValues }: TaskDetailsProps) => {
+  // const [assignee, setAssignee] = useState({})
+  const {
+    // id,
+    assignee,
+    title,
+    description,
+    // status,
+    dueDate,
+    // creationDate,
+    project
+  } = task;
+
+  // useEffect(() => {
+
+  // }, [])
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form className="m-3" onSubmit={handleSubmit}>
+      <div className="form-group row">
+        <div className="col-sm-12">
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            placeholder="Task's Title"
+            defaultValue={title}
+          />
         </div>
-    )
+      </div>
+      <div className="form-group row">
+        <label
+          className="col-sm-2 col-form-label"
+          htmlFor="exampleFormControlSelect1"
+        >
+          Assignee
+        </label>
+        <div className="col-sm-10">
+          <div className="row m-1">
+            <div className="pic-area mr-1">
+              <img
+                style={{ width: "40px", height: "40px", borderRadius: "50%" }}
+                src="https://cangeo-media-library.s3.amazonaws.com/s3fs-public/styles/web_article_slider_image/public/images/web_articles/article_images/3120/eastern_chipmunk.jpg?itok=vpESnz24"
+              />
+            </div>
+            <select
+              className="form-control"
+              id="assignee"
+              style={{ width: "auto", flexGrow: 1 }}
+            >
+              <option value={1}>Assignee 1</option>
+              <option value={2}>Assignee 2</option>
+              <option value={3}>Assignee 3</option>
+              <option value={4}>Assignee 4</option>
+              <option value={5}>Assignee 5</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div className="form-group row">
+        <label
+          className="col-sm-2 col-form-label"
+          htmlFor="exampleFormControlTextarea1"
+        >
+          Example textarea
+        </label>
+        <div className="col-sm-10">
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows={3}
+            placeholder={description}
+          ></textarea>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default TaskDetails;
