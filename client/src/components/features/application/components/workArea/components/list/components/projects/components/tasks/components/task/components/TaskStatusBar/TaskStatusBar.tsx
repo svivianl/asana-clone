345037ae3d334c46fca3fd79e2a9599a424f5943
Viviane <vshibukawa@spiria.com>
2020-03-26 09:59:24 -0400

    created Task component

---
+++ client/src/components/features/application/components/workArea/components/list/components/projects/components/tasks/components/task/components/TaskStatusBar/TaskStatusBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TaskStatusBar = () => {
+
+
+    return(
+        <div className='jumbotron'>
+           
+        </div>
+    )
+};
+
+export default TaskStatusBar;
