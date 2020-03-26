3ba4e26041cb731cdcbbff20ff9c889be9355aaf
Viviane <vshibukawa@spiria.com>
2020-03-26 08:28:49 -0400

    testing sass

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Web app to help teams organize tasks and projects
 
 ## How to run
 
-In ```client``` folder, run:
+In the ```client```'s folder, run:
 * ```npm run scss```
 * ```npm run server```
 * ```npm start```
--- client/src/components/features/application/components/workArea/components/layout/Layout.tsx
+++ client/src/components/features/application/components/workArea/components/layout/Layout.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import '../../../../../../../css/components/features/application/components/workArea/components/layout/layout.css'
 
 const Layout = () => {
 
   return (
-    <h3>Layout</h3>
+    <div className='h5-sass'>Layout</div>
   )
 }
 
